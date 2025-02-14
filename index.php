6ac1dd85c319c5c46368cc5c73c522a50414deaa
Georges Kmeid <georgesk117@gmail.com>
2025-02-14 12:38:50 +0700

    Make required changes to ensure code is functional

--- index.php
+++ index.php
@@ -6,13 +6,22 @@ function isValidPhoneNumber($phone_number, $customer_id, $api_key)
 
     $headers = [
         "Authorization: Basic " . base64_encode("$customer_id:$api_key"),
-        "Content-Type: application/x-www-form-urlencoded"
+        "Content-Type: application/json" // Ensures the API correctly interprets the request as JSON
     ];
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $api_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $data = json_encode([
+        "consent" => ["method" => 1],
+    ]);
+
+    /* According to Telesign API docs, the method is POST, and this line sends the JSON-encoded data
+       as the body of the POST request to the API. */
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
     $response = curl_exec($ch);
     $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -23,12 +32,12 @@ function isValidPhoneNumber($phone_number, $customer_id, $api_key)
     }
 
     $data = json_decode($response, true);
-    if (!isset($data['numbering']['phone_type'])) {
+    if (!isset($data['phone_type']['description'])) { // Changed to match API docs, which use phone_type.description.
         return false; // Unexpected API response
     }
 
     $valid_types = ["FIXED_LINE", "MOBILE", "VALID"];
-    return in_array(strtoupper($data['numbering']['phone_type']), $valid_types);
+    return in_array(strtoupper($data['phone_type']['description']), $valid_types);
 }
 
 // Usage example
